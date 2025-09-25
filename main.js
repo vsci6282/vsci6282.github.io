85f5ac46efd972a3bb30e677f6a4b2194aa0ae37
vsci6282 <vsci6282@gmail.com>
2025-09-24 21:50:54 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,159 @@
+let selectedNum = 0;
+let previousNum = 0;
+const notes = ['c', 'd', 'e', 'f', 'g', 'a', 'b'];
+const trumpetNotesDictionary = {c: 0, d: 5, e: 6, f: 4, g: 0, a: 6, b: 2};
+const key1 = document.getElementById("key1");
+let key1down = false;
+const key2 = document.getElementById("key2");
+let key2down = false;
+const key3 = document.getElementById("key3");
+const resultPColor = document.getElementById("result");
+let pp = false;
+let keyBdown = false;
+let selectedInt = 0;
+let num = 0;
+
+main()
+
+document.addEventListener('keydown', (event) => {
+        if (event.key === 'j') {
+          if (!key1down) {
+            key1down = true;
+          }
+        }
+        if (event.key === 'k') {
+          if (!key2down) {
+            key2down = true;
+          }
+        }
+        if (event.key === 'l') {
+          if (!key3down) {
+            key3down = true;
+          }
+        }
+        if (event.key === 'b') {
+          if (!keyBdown) {
+            keyBdown = true;
+            checkResult();
+          }
+        }
+        getKeys();
+        //document.getElementById("num").innerHTML = selectedInt;
+    });
+
+document.addEventListener('keyup', (event) => {
+        if (event.key === 'j') {
+          key1down = false;
+          //updateSelectedInt(-4);
+        }
+        if (event.key === 'k') {
+          key2down = false;
+          //updateSelectedInt(-2);
+        }
+        if (event.key === 'l') {
+          key3down = false;
+          //updateSelectedInt(-1);
+        }
+        if (event.key === 'b') {
+          keyBdown = false;
+          checkResult();
+        }
+        getKeys();
+    });
+
+function main() {
+  pp = false
+  selectedNum = Math.floor(Math.random()*7);
+
+  /*if (selectedNum == previousNum) {
+    while (selectedNum == previousNum) {
+      previousNum = selectedNum;
+      selectedNum = Math.floor(Math.random()*7);
+    }
+  }*/
+
+  document.getElementById("selected note").innerHTML = notes[selectedNum];
+
+  resetResult()
+  //document.getElementById("note").innerHTML = trumpetNoteToNum(notes[num]);
+  //document.getElementById("test").innerHTML = selectedNote;
+}
+
+function getKeys() {
+  selectedInt = 0;
+  if (!pp) {
+    if (key1down) {
+      selectedInt += 4;
+    }
+    if (key2down) {
+      selectedInt += 2;
+    }
+    if (key3down) {
+      selectedInt += 1;
+    }
+  }
+  //document.getElementById("num").innerHTML = selectedInt;
+}
+
+function checkResult() {
+  if (trumpetNotesDictionary[notes[selectedNum]] == selectedInt || pp) {
+    document.getElementById("result").innerHTML = "correct!";
+    resultPColor.style.color = 'green';
+    pp = true;
+    setTimeout(main, 100);
+  } else {
+    document.getElementById("result").innerHTML = "incorrect";
+    resultPColor.style.color = 'red';
+  }
+}
+
+function resetResult() {
+  document.getElementById("result").innerHTML = "";
+}
+
+function numtonote(num) {
+  return notes[num];
+}
+
+function trumpetNoteToNum(note) {
+  return trumpetNotesDictionary[note]
+}
+
+function updateSelectedInt(num) {
+  selectedInt += num;
+  if (selectedInt < 0) {
+    selectedInt = 0;
+  }
+  document.getElementById("num").innerHTML = selectedInt;
+}
+
+function playKey(keyNum) {
+  if (keyNum == 1) {
+    if (key1.checked) {
+      selectedInt += 4;
+    } else {
+      selectedInt -= 4;
+    }
+  }
+  if (keyNum == 2) {
+    if (key2.checked) {
+      selectedInt += 2;
+    } else {
+      selectedInt -= 2;
+    }
+  }
+  if (keyNum == 3) {
+    if (key3.checked) {
+      selectedInt += 1;
+    } else {
+      selectedInt -= 1;
+    }
+  }
+  //document.getElementById("num").innerHTML = selectedInt;
+
+}
+
+function test() {
+  //e = "b";
+  //document.getElementById("test").innerHTML = e;
+}
